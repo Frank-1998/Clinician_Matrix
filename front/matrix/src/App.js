0c04335ec0efd899b1a6cc4639329003c76dfddd
LubabaSheikh <lubaba.sheikh01@gmail.com>
2023-11-19 04:20:28 -0700

    front end changes

--- front/matrix/src/App.js
+++ front/matrix/src/App.js
@@ -1,16 +1,38 @@
-import logo from './logo.svg';
+import logo from './images/logoText.png';
+import Buttons from './components/Buttons';
 import './App.css';
+import PersonAddAltRoundedIcon from '@mui/icons-material/PersonAddAltRounded';
+import AccountCircleRoundedIcon from '@mui/icons-material/AccountCircleRounded';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          HELLO
-        </p>
-        <a
+      <div className="landing-page-section">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h2>Welcome!</h2>
+      <div className="flex-container">
+        <div className="flex-container">
+          <h4>Getting Started?</h4>
+          <div className="button">
+            <Buttons label="Signup As Nurse" icon={<PersonAddAltRoundedIcon />} />
+          </div>
+          <div className="button">
+            <Buttons label="Signup As Manager" icon={<PersonAddAltRoundedIcon />} />
+          </div>
+        </div>
+        <div className="flex-container">
+          <h4>Returning User?</h4>
+          <div>
+            <Buttons label="Login As Nurse" icon={<AccountCircleRoundedIcon />} />
+          </div>
+          <div>
+            <Buttons label="Login As Manager" icon={<AccountCircleRoundedIcon />} />
+          </div>
+        </div>
+      </div>
+    </div>
+      <a
           className="App-link"
           //href="https://reactjs.org"
           href="/login"
